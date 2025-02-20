8fe18b808837f3af6da8203ab9f89f629639f7de
moot moot <ehluu@ucsc.edu>
2025-02-20 15:01:57 -0800

    template from html5up

--- twt.py
+++ twt.py
@@ -1,3 +1,8 @@
-ligma = input('enter twitter url: ')
-x = ligma[20:].split("/")
-print(f'@{x[0]}_{x[2]}')
+import pyperclip
+while(1):
+    ligma = input('enter twitter url: ')
+    x = ligma[14:].split("/")
+    # print(f'@{x[0]}_{x[2]}')
+    b = '@' + x[0] + '_' + x[2]
+    # print(b)
+    pyperclip.copy(b)
